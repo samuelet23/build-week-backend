154bb757e04d02a6b925d94576821bcbf5b8a61b
Maudit00 <davide9311@gmail.com>
2024-01-29 17:30:53 +0100

    creazione dao classes

--- src/main/java/dao/PuntiDiEmissioneDAO.java
+++ src/main/java/dao/PuntiDiEmissioneDAO.java
@@ -2,10 +2,7 @@ package dao;
 
 import entities.PuntiDiEmissione;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import jakarta.persistence.*;
 
 public class PuntiDiEmissioneDAO {
     private EntityManagerFactory emf;
